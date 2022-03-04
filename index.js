8cf784f23acd298579ba538d21ff7d29ff3a849d
Rontu22 <rontubarhoi01@gmail.com>
2022-03-04 14:28:35 +0530

    Added all files

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("First");
+import fs from "fs";
+
+fs.writeFile("log", "Hi there", (err) => {
+  if (err) throw err;
+  console.log("its saved");
+});
+
+const saveLog = (logMessage) => {
+  let location = "/var/log/syslog";
+  fs.appendFile(`${location}`, `${logMessage}`, (err) => {
+    if (err) console.log(err);
+  });
+};
+
+saveLog("Hi");
